168bfbd483fc99e9291347e77b35969c03b10063
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-16 11:20:14 +0300

    Started a new project

---
+++ Angela Project 1 Storyboard/Angela Project 1 Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Angela Project 1 Storyboard
+//
+//  Created by Арсентий Халимовский on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
