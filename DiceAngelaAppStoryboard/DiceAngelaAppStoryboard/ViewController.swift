3fe779e109616f0ca626096a7bad95d2c54a0fa9
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-16 17:55:10 +0300

    Started a new project

---
+++ DiceAngelaAppStoryboard/DiceAngelaAppStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceAngelaAppStoryboard
+//
+//  Created by Арсентий Халимовский on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
