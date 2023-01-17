94cce20d618e8178aeb365d33601ff1cbd2a3933
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-17 10:10:46 +0300

    small changes of the image view

--- DiceAngelaAppStoryboard/DiceAngelaAppStoryboard/ViewController.swift
+++ DiceAngelaAppStoryboard/DiceAngelaAppStoryboard/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // change of the current image
+        diceImageView1.image = UIImage(named: "dice6")
+        // change of the current alfa of the image
+        diceImageView1.alpha = 0.5
     }
 
 
