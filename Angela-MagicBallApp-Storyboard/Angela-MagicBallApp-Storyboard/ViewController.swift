6c52fb5b22d19b9e034d07c1c61682621ef6c7b3
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-17 16:25:04 +0300

    Implemented GCD
    ball will answer after playing the sound

--- Angela-MagicBallApp-Storyboard/Angela-MagicBallApp-Storyboard/ViewController.swift
+++ Angela-MagicBallApp-Storyboard/Angela-MagicBallApp-Storyboard/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var ballAnswer: UIImageView!
     
+    // added entity for audio player
     var player: AVAudioPlayer?
     
     let arrayOfTheAnswers =
@@ -32,22 +33,32 @@ class ViewController: UIViewController {
         
         playSound()
         
-        changeBallAnswer()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: changeBallAnswer)
+        
     }
     
+    // made separate function for answers of the ball
     func changeBallAnswer() {
         ballAnswer.image = arrayOfTheAnswers.randomElement()!
     }
     
+    // separate function for playing the specific sound after button was pressed.
     func playSound() {
         
+        // 1. let's check the content of our app. Is it has needed file or not:
         guard let path = Bundle.main.path(forResource: "sound", ofType: "mp3") else { return }
+        
+        // 2. Use our sound file to make a specific url for our audio player:
         let url = URL(fileURLWithPath: path)
         
+        // 3. Special do-catch block to try if there is a real sound and it's working correctly
         do {
+            // 3.1 Load our sound to AudioPlayer
             player = try AVAudioPlayer(contentsOf: url)
+            // 3.2 Play the sound
             player?.play()
             
+            // 3.3 If it's not let's throw the error and print it's description
         } catch let error {
             print(error.localizedDescription)
         }
