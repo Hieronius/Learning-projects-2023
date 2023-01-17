2378710a428c06fc422788b116734368e70ae145
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-17 10:45:47 +0300

    Added new button

--- DiceAngelaAppStoryboard/DiceAngelaAppStoryboard/ViewController.swift
+++ DiceAngelaAppStoryboard/DiceAngelaAppStoryboard/ViewController.swift
@@ -9,18 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // outlets for our dices
     @IBOutlet weak var diceImageView1: UIImageView!
-    
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // change of the current image
+        // WHO         WHAT  =   VALUE
         diceImageView1.image = UIImage(named: "dice6")
+        
         // change of the current alfa of the image
         diceImageView1.alpha = 0.5
+        
+        // change of the current image of the second dice
+        diceImageView2.image = UIImage(named: "dice2")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped")
+    }
+    
 }
 
