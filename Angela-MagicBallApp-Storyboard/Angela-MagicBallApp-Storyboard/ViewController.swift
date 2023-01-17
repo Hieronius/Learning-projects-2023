af3ab9e349b916c8c0a33659a5c41329f3ac32bc
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-17 15:55:53 +0300

    Created two methods
    to play sound after you press the button and change the ball answer after it

--- Angela-MagicBallApp-Storyboard/Angela-MagicBallApp-Storyboard/ViewController.swift
+++ Angela-MagicBallApp-Storyboard/Angela-MagicBallApp-Storyboard/ViewController.swift
@@ -4,16 +4,52 @@
 //
 //  Created by Арсентий Халимовский on 17.01.2023.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballAnswer: UIImageView!
+    
+    var player: AVAudioPlayer?
+    
+    let arrayOfTheAnswers =
+        [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5")
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func askQuestion(_ sender: UIButton) {
+        
+        playSound()
+        
+        changeBallAnswer()
+    }
+    
+    func changeBallAnswer() {
+        ballAnswer.image = arrayOfTheAnswers.randomElement()!
+    }
+    
+    func playSound() {
+        
+        guard let url = Bundle.main.url(forResource: "sound", withExtension: "mp3") else { return }
+        
+        do {
+            let player = try AVAudioPlayer(contentsOf: url)
+            
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
