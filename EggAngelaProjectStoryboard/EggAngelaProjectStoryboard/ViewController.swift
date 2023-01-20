6c11d7dd8632896a1b2a515c1b0a7c014012dc86
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-20 16:36:13 +0300

    Fixed timer
    no it will reset each time when we press a button

--- EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
+++ EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
@@ -15,11 +15,19 @@ class ViewController: UIViewController {
     
     // Needed time for custom countdown
     var counter = 60
+    
+    // here i should define an instance of the Timer class. One for all project
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    func setCountDown() {
         // custom timer
+        // We should define our timer property with specific settings:
         // Timer - it's a class for defining any timers
         // scheduledTimer - mean the specific options of timer
         // timer inverval - how fast timer will go
@@ -27,16 +35,21 @@ class ViewController: UIViewController {
         // selector - specific function of method to run each interval
         // userInfo - i think something like documentary
         // repeats - one time or more we wan't to repeat our timer
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
     }
     
+    // objc method to update our timer
     @objc func updateCounter() {
+        
+        
         if counter > 0 {
             print("\(counter) seconds to the end of the timer")
             counter -= 1
         }
     }
     
+    // here we use print that depends of current state of the pressed button
     func yolkCheck() {
         switch hardness {
         case "Soft":
@@ -50,12 +63,23 @@ class ViewController: UIViewController {
         }
     }
 
-
+    // our button to press
     @IBAction func typeOfTheEgg(_ sender: UIButton) {
         
+        // each new click of the button we won't reser our timer
+        timer.invalidate()
+        
+        // and set the counter number to default:
+        counter = 60
+        
+        // here we wan't to check the name of the button that was pressed
         hardness = sender.currentTitle!
         
+        // here we wan't to check the time needed for cooking a specific type of the egg.
         yolkCheck()
+        
+        // here we wan't to start our timer
+        setCountDown()
         
     }
 }
