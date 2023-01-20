c5d6b23df10d69c4767b23d678abcbc690495c47
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-20 13:18:37 +0300

    Implemented switch
    Depends of button name we can see the time which required for cooking this or that type of the egg.

--- EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
+++ EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +20,20 @@ class ViewController: UIViewController {
 
 
     @IBAction func typeOfTheEgg(_ sender: UIButton) {
-        print(sender.titleLabel?.text!)
+        
+        let hardness = sender.currentTitle!
+        
+        switch hardness {
+        case "Soft":
+            print("the time of cooking for soft yolk is \(softTime) minutes")
+        case "Medium":
+            print("the time of cooking for medium yolk is \(mediumTime) minutes")
+        case "Hard":
+            print("the time of cooking for hard yolk is \(hardTime) minutes")
+        default:
+            print("Unknown state")
+        }
+        
     }
 }
 
