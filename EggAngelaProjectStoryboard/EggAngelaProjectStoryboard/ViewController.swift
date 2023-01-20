ee38d909b0f08a6c64a25f72779d151feafa7a2f
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-20 15:02:24 +0300

    Implemented a timer to the app

--- EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
+++ EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
@@ -12,10 +12,29 @@ class ViewController: UIViewController {
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     var hardness = ""
+    
+    // Needed time for custom countdown
+    var counter = 60
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // custom timer
+        // Timer - it's a class for defining any timers
+        // scheduledTimer - mean the specific options of timer
+        // timer inverval - how fast timer will go
+        // target - this or different view controller
+        // selector - specific function of method to run each interval
+        // userInfo - i think something like documentary
+        // repeats - one time or more we wan't to repeat our timer
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if counter > 0 {
+            print("\(counter) seconds to the end of the timer")
+            counter -= 1
+        }
     }
     
     func yolkCheck() {
