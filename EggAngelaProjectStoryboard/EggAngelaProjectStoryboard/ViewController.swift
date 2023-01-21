19289d0b03c04e3e94e234244c6599da1180262a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-21 09:17:26 +0300

    Tried to fix progress bar

--- EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
+++ EggAngelaProjectStoryboard/EggAngelaProjectStoryboard/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
+    
+    // our progress bar
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    // title outlet for our label
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    // array of all possible yolk types
+    let eggTimes = ["Soft": 3, "Medium": 100, "Hard": 5]
+    
+    // type of the yolk
     var hardness = ""
     
     // Needed time for custom countdown
@@ -18,12 +28,9 @@ class ViewController: UIViewController {
     
     // here i should define an instance of the Timer class. One for all project
     var timer = Timer()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-    }
+    
+    // current state of the progress bar
+    var progress = 0
     
     func setCountDown() {
         // custom timer
@@ -39,15 +46,49 @@ class ViewController: UIViewController {
         
     }
     
+    // func to set our progress bar
+    func setProgressBar() {
+        
+        // Here will be my work in 21.01.23
+        
+        // 1 % of the total progress will be equal to:
+        let onePersentOfProgress = Float(counter / 100)
+        
+        // print current amount of points of 1%
+        print(onePersentOfProgress)
+        
+        // current progress:
+        progressBar.progress = 0.0
+        
+        // adding progress
+        if counter > -1 {
+            print("\(progressBar.progress) to end this timer")
+            progressBar.progress += 1.0
+        }
+    }
+    
     // objc method to update our timer
     @objc func updateCounter() {
         
+        
         // here we wan't be sure that time won't go below 0 because there is no such a thing like a minus time
         // so each second we wan't to subtract one second from the timer number
-        if counter > 0 {
+        if counter > -1 {
+            // print how much seconds left
             print("\(counter) seconds to the end of the timer")
+            
+            // set label text as our current amount of second to end
+            labelTitle.text = "\(counter)"
+            
+            // deduct one point from the timer
             counter -= 1
+            
+            // added special check if counter equal to zero or less let's print a message
+            if counter == -1 {
+                labelTitle.text = "Your egg is cooked"
+            }
         }
+        
     }
     
     // here we use print that depends of current state of the pressed button
@@ -81,6 +122,9 @@ class ViewController: UIViewController {
         
         // here we wan't to start our timer
         setCountDown()
+        
+        // here we wan't to start our progress bar
+        setProgressBar()
         
     }
 }
