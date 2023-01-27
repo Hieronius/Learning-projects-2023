3e4036001f104235187a4c180ab1de82b1184be7
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-27 19:40:20 +0300

    create struct for questions

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -7,6 +7,19 @@
 
 import UIKit
 
+// Creation of struct of questions and asnwers
+struct Question {
+    var question = ""
+    var answer = ""
+}
+
+// making some properties of QuestionStruct:
+var firstQuestion = Question(question: "Four + Two is equal to Six", answer: "True")
+var secondQuestion = Question(question: "Five - Three is greater than One", answer: "True")
+var thirdQuestion = Question(question: "Three + Eight is less than Ten", answer: "False")
+
+
+
 class ViewController: UIViewController {
     
     // list of the objects from storyboard:
@@ -18,18 +31,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var falseButton: UIButton!
     
+    // text for our questionLabel
     var question = "" {
         didSet {
             questionLabel.text = "\(question)"
         }
     }
     
-    let quiz = [
-        ["Four + Two is equal to Six", "True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten", "False"]
-        ]
+    // array of the questions and answers to them
+    var quiz = [firstQuestion, secondQuestion, thirdQuestion]
+
+//    let quiz = [
+//        ["Four + Two is equal to Six", "True"],
+//        ["Five - Three is greater than One", "True"],
+//        ["Three + Eight is less than Ten", "False"]
+//        ]
     
+    // current question number
     var questionNumber = 0
     
     override func viewDidLoad() {
@@ -48,24 +66,31 @@ class ViewController: UIViewController {
         let userAnswer = sender.currentTitle
         
         // detection of what answer is really true or false:
-        let actualAnswer = quiz[questionNumber][1]
+        let actualAnswer = quiz[questionNumber].answer
         
+        // check user's answer and current right answer:
         if userAnswer == actualAnswer {
             print("right")
         } else {
             print("Wrong")
         }
         
-        // open the next question of quiz array
-        questionNumber += 1
-        // display this question on the view
-        updateUI()
+        // check if there is no element which out of range
+        if questionNumber + 1 < quiz.count {
+            // if there is no problem let's push the next question
+            questionNumber += 1
+        } else {
+            // in other way let's start from zero
+            questionNumber = 0
+        }
+            // display this question on the view
+            updateUI()
        
     }
     
     // function to reload our questions
     func updateUI() {
-        question = quiz[questionNumber][0]
+        question = quiz[questionNumber].question
         
         
     }
