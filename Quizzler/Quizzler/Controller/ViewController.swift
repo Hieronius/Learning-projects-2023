93d1cfbc9b763c032dd38b8fa30d1507ab0b2341
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-28 01:11:08 +0300

    Remodel app to MVP pattern

--- Quizzler/Quizzler/Controller/ViewController.swift
+++ Quizzler/Quizzler/Controller/ViewController.swift
@@ -25,24 +25,8 @@ class ViewController: UIViewController {
         }
     }
     
-    // array of the questions and answers to them
-    var quiz = [
-            Question(q: "A slug's blood is green.", a: "True"),
-            Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-            Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-            Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-            Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-            Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-            Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-            Question(q: "Google was originally called 'Backrub'.", a: "True"),
-            Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-            Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-            Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-            Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    ]
-    
-    // current question number
-    var questionNumber = 0
+    // Property of our logic center of the app
+    var quizBrain = QuizBrain()
     
     
     override func viewDidLoad() {
@@ -64,8 +48,7 @@ class ViewController: UIViewController {
         // detection of which button user will press:
         let userAnswer = sender.currentTitle
         
-        // detection of what answer is really true or false:
-        let actualAnswer = quiz[questionNumber].answer
+        quizBrain.checkAnswer()
         
         // check user's answer and current right answer:
         if userAnswer == actualAnswer {
