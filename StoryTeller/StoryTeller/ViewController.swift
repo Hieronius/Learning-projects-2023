ad4dd0d7f0fdc92e8c702927e8eab09369ac093d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-29 16:57:18 +0300

    Create a new project

---
+++ StoryTeller/StoryTeller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryTeller
+//
+//  Created by Арсентий Халимовский on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
