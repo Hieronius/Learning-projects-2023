7113c2aa91e06457e6c0090954a94770e2754ded
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-30 13:40:22 +0300

    add outlets for elements

---
+++ StoryTeller/StoryTeller/StoryBrain.swift
@@ -0,0 +1,9 @@
+//
+//  StoryBrain.swift
+//  StoryTeller
+//
+//  Created by Арсентий Халимовский on 30.01.2023.
+//
+
+import Foundation
+import UIKit
--- StoryTeller/StoryTeller/ViewController.swift
+++ StoryTeller/StoryTeller/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBOutlet weak var rightButton: UIButton!
+    
+    @IBOutlet weak var leftButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
