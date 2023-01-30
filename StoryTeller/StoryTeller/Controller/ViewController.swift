96440277e360b17118f7f19215fb1bf80cc389d2
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-30 14:57:36 +0300

    Define some functions
    for new story part and for user answer check

--- StoryTeller/StoryTeller/Controller/ViewController.swift
+++ StoryTeller/StoryTeller/Controller/ViewController.swift
@@ -14,14 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
     
+    // text which displays right now:
+    var currentStory = "" {
+        didSet {
+            storyLabel.text = "\(currentStory)"
+        }
+    }
+    
+    // property of our Story struct on the View Controller so we can have access to it here
+    var story = Story()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     // action that happen when we press the buttons:
-    @IBAction func pressButtons(_ sender: Any) {
+    @IBAction func pressButtons(_ sender: UIButton) {
         
+        // let's detect what choice user has made:
+        let userChoice = sender.currentTitle
+        
+        // now we should check choice with possible choices from Story array
     }
 }
 
