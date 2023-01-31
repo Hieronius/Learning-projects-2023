fd441574aed1cb3429ed51b8f20ae0a34afe1112
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-31 14:40:07 +0300

    Fix checkAnswer method

--- StoryTeller/StoryTeller/Model/StoryBrain.swift
+++ StoryTeller/StoryTeller/Model/StoryBrain.swift
@@ -11,6 +11,8 @@ import UIKit
 struct StoryBrain {
     
     var stories = [
+        
+        // Story. Part 0
         Story(
             title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.",
             choice1: "I'll hop in. Thanks for the help!",
@@ -18,6 +20,8 @@ struct StoryBrain {
             choice2: "Better ask him if he's a murderer first.",
             choice2Destination: 1
         ),
+        
+        // Story. Part 1.
         Story(
             title: "He nods slowly, unfazed by the question.",
             choice1: "At least he's honest. I'll climb in.",
@@ -25,6 +29,8 @@ struct StoryBrain {
             choice2: "Wait, I know how to change a tire.",
             choice2Destination: 3
         ),
+        
+        // Story. Part 2.
         Story(
             title: "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.",
             choice1: "I love Elton John! Hand him the cassette tape.",
@@ -32,6 +38,8 @@ struct StoryBrain {
             choice2: "It's him or me! You take the knife and stab him.",
             choice2Destination: 4
         ),
+        
+        // Story. Part 3.
         Story(
             title: "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?",
             choice1: "The",
@@ -39,6 +47,8 @@ struct StoryBrain {
             choice2: "End",
             choice2Destination: 0
         ),
+        
+        // Story. Part 4.
         Story(
             title: "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.",
             choice1: "The",
@@ -46,6 +56,8 @@ struct StoryBrain {
             choice2: "End",
             choice2Destination: 0
         ),
+        
+        // Story. Part 5.
         Story(
             title: "You bond with the murderer while crooning verses of 'Can you feel the love tonight'. He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: 'Try the pier.'",
             choice1: "The",
@@ -67,9 +79,9 @@ struct StoryBrain {
     // if uses choose second choice let's return second destination
     func checkAnswer(_ userChoice: String) -> Int {
         if userChoice == stories[textPartNumber].choice1 {
-            return 1
+            return stories[textPartNumber].choice1Destination
         } else {
-            return 2
+            return stories[textPartNumber].choice2Destination
         }
     }
     
