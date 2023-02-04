6db86a6ad3498879d875c2d11627a62c6a08ada8
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-04 22:02:28 +0300

    Change color of SuperView

--- BMI calculator/BMI calculator/Controllers/SecondViewController.swift
+++ BMI calculator/BMI calculator/Controllers/SecondViewController.swift
@@ -12,13 +12,17 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new label
+        // define background color for superView:
+        view.backgroundColor = .red
+        
+        // create a new label:
         let label = UILabel()
         label.text = "Hello"
         
         // location of our label:
         label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
         
-        
+        // add label to superView:
+        view.addSubview(label)
     }
 }
