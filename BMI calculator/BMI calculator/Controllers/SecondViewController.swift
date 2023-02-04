1e58ca9289300206e247124bcd53be8bfd9f5646
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-04 21:58:13 +0300

    Create a label on second view controller

--- BMI calculator/BMI calculator/Controllers/SecondViewController.swift
+++ BMI calculator/BMI calculator/Controllers/SecondViewController.swift
@@ -5,4 +5,20 @@
 //  Created by Арсентий Халимовский on 04.02.2023.
 //
 
-import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new label
+        let label = UILabel()
+        label.text = "Hello"
+        
+        // location of our label:
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        
+    }
+}
