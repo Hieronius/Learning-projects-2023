49e303245e9ad406e87df26d1bd21b844e7536b5
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-05 16:14:46 +0300

    Create a new VC

---
+++ BMI calculator/BMI calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultViewController.swift
+//  BMI calculator
+//
+//  Created by Арсентий Халимовский on 05.02.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func recalculateButton(_ sender: UIButton) {
+    }
+    
+}
