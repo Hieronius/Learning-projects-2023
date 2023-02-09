60ddd5681df27e737556fb30f2e99cedeef17da2
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-09 13:46:26 +0300

    Create stackViewPractice project

---
+++ StackViewPractice/StackViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewPractice
+//
+//  Created by Арсентий Халимовский on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
