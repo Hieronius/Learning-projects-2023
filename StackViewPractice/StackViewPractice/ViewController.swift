0f72a3db4d6decd7d19a92f814a187b52ed24a96
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-09 13:52:11 +0300

    Add stack view and image view

--- StackViewPractice/StackViewPractice/ViewController.swift
+++ StackViewPractice/StackViewPractice/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createStackView()
+        
     }
 
+    func createStackView() {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemGreen
+        imageView.image = UIImage(systemName: "bell")
+        imageView.contentMode = .scaleAspectFit
+        imageView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView])
+        stackView.frame = view.bounds
+        view.addSubview(stackView)
+    }
 
 }
 
