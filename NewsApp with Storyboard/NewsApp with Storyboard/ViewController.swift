80bee0198111dc4993e71005ef15a2722ea8ac9c
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-13 21:01:05 +0300

    Start news app project

---
+++ NewsApp with Storyboard/NewsApp with Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp with Storyboard
+//
+//  Created by Арсентий Халимовский on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
