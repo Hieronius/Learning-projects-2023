b4e80bdc980c7325a325f0ea6b24a46812615e3b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-15 22:55:41 +0300

    Add UITabbarController

---
+++ NewsApp with Storyboard/NewsApp with Storyboard/Tabbar/TabbarController.swift
@@ -0,0 +1,12 @@
+//
+//  TabbarController.swift
+//  NewsApp with Storyboard
+//
+//  Created by Арсентий Халимовский on 15.02.2023.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+
+}
