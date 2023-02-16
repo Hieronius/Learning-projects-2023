e8f417d6deb4c6d146dbfa1512b7dac73af01103
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-16 15:26:16 +0300

    Add outlets for elements

--- SB Hello world/SB Hello world/ViewController.swift
+++ SB Hello world/SB Hello world/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
+    @IBOutlet weak var toogleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func makeButtonAction(_ sender: Any) {
+    }
 }
 
