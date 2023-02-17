d0a4075c19171c1a558e36760e07ddd627345a7d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-17 16:50:00 +0300

    add switch for segment controll

--- SB UIKit app/SB UIKit app/ViewController.swift
+++ SB UIKit app/SB UIKit app/ViewController.swift
@@ -17,9 +17,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        segmentControll.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 0
     }
 
     @IBAction func segmentControlAction(_ sender: Any) {
+        switch segmentControll.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "Выбран сегмент с индексом 0"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "Выбран сегмент с индексом 1"
+            mainLabel.textColor = .yellow
+        case 2:
+            mainLabel.text = "Выбран сегмент с индексом 2"
+            mainLabel.textColor = .green
+        default:
+            break
+        }
     }
     
 }
