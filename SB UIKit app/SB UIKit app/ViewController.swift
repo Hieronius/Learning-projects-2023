8728eb495e2dc58c3ce9e4830731d9a98047469a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-17 16:10:55 +0300

    add outlets for elements

--- SB UIKit app/SB UIKit app/ViewController.swift
+++ SB UIKit app/SB UIKit app/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentControll: UISegmentedControl!
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func segmentControlAction(_ sender: Any) {
+    }
+    
 }
 
