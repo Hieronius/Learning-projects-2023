40cb8340038410b7ade03d63e35fe86552300594
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-19 14:00:41 +0300

    Add Detail VC

---
+++ SB NavigationApp/SB NavigationApp/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  SB NavigationApp
+//
+//  Created by Арсентий Халимовский on 18.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var testVariable = 2
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- SB NavigationApp/SB NavigationApp/ViewController.swift
+++ SB NavigationApp/SB NavigationApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var detail = DetailViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func useSegue(_ sender: Any) {
+        performSegue(withIdentifier: "showDetail", sender: sender)
+        
+        detail.testVariable = 5
+        print(detail.testVariable)
+    }
 
 }
 
