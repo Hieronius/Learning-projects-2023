e4a1c3111dda6d690a4ffc3ac87b97caa27b1ee4
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-20 19:13:17 +0300

    Add set of questions

--- SB AnimalQuiz/SB AnimalQuiz/Models/Answer.swift
+++ SB AnimalQuiz/SB AnimalQuiz/Models/Answer.swift
@@ -5,14 +5,29 @@
 //  Created by Арсентий Халимовский on 20.02.2023.
 //
 
+
+// this file will be behind the scenes of the last "result screen"
 struct Answer {
     let text: String
     let type: AnimalType
 }
 
-enum AnimalType: String {
+enum AnimalType: Character {
     case dog = "🐶"
     case cat = "🐱"
     case rabbit = "🐰"
     case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Собака"
+        case .cat:
+            return "Кошка"
+        case .rabbit:
+            return "Кролик"
+        case .turtle:
+            return "Черепаха"
+        }
+    }
 }
--- SB AnimalQuiz/SB AnimalQuiz/Models/Question.swift
+++ SB AnimalQuiz/SB AnimalQuiz/Models/Question.swift
@@ -16,3 +16,33 @@ struct Question {
     let type: ResponseType
     let answer: [Answer]
 }
+
+extension Question {
+    static func getQuest() -> [Question] {
+        return [
+            Question(text: "Какую пищу вы предпочитаете?",
+            type: .single,
+            answer: [
+                Answer(text: "Стейк", type: .dog),
+                Answer(text: "Рыба", type: .cat),
+                Answer(text: "Морковь", type: .rabbit),
+                Answer(text: "Кукуруза", type: .turtle)
+                ]
+        ),
+            Question(text: "Что вы любите делать?", type: .single, answer: [
+                Answer(text: "Плавать", type: .dog),
+                Answer(text: "Спать", type: .cat),
+                Answer(text: "Обниматься", type: .rabbit),
+                Answer(text: "Есть", type: .turtle)
+            ]
+        ),
+            Question(text: "Любите ли вы поездки на машине", type: .single, answer: [
+                Answer(text: "Обожаю", type: .dog),
+                Answer(text: "Обожаю", type: .cat),
+                Answer(text: "Ненавижу", type: .rabbit),
+                Answer(text: "Ненавижу", type: .turtle)
+            ]
+        )
+       ]
+    }
+}
