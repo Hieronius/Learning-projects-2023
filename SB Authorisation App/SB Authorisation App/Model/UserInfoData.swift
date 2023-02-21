76e9654448a8aa183ff554d56ffc3d0627ca8fdf
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-21 19:08:10 +0300

    add userInfo model

---
+++ SB Authorisation App/SB Authorisation App/Model/UserInfoData.swift
@@ -0,0 +1,13 @@
+//
+//  UserInfoData.swift
+//  SB Authorisation App
+//
+//  Created by Арсентий Халимовский on 21.02.2023.
+//
+
+import UIKit
+
+struct UserInfo {
+    let login = "User"
+    let password = "Password"
+}
