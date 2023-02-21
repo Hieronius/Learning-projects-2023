bf9bf3a9b9f01f5bc4a2293f54f840a7420309d7
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-21 18:57:37 +0300

    add start and profile VC

---
+++ SB Authorisation App/SB Authorisation App/View Controllers/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  SB Authorisation App
+//
+//  Created by Арсентий Халимовский on 21.02.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // write some code here
+    }
+}
---
+++ SB Authorisation App/SB Authorisation App/View Controllers/StartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartViewController.swift
+//  SB Authorisation App
+//
+//  Created by Арсентий Халимовский on 21.02.2023.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // text some code here
+    }
+}
