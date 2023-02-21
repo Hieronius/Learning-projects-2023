48a280c4a0e7937678d2efd65fe39dc16126a982
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-21 15:32:42 +0300

    Create a new project

---
+++ SB Authorisation App/SB Authorisation App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB Authorisation App
+//
+//  Created by Арсентий Халимовский on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
