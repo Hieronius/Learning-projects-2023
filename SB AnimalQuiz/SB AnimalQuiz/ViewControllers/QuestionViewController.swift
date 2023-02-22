93c4a4dfa38f2728f70ac6173c70d963519ece1d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-22 18:36:57 +0300

    Add some code for questions vc
    Set elements to be hidden when there is not right question and open it when new question will start.

--- SB AnimalQuiz/SB AnimalQuiz/ViewControllers/QuestionViewController.swift
+++ SB AnimalQuiz/SB AnimalQuiz/ViewControllers/QuestionViewController.swift
@@ -25,7 +25,11 @@ class QuestionViewController: UIViewController {
     @IBOutlet var rangedLabels: [UILabel]!
     
     private let questions = Question.getQuestions()
-    private var currentIndex = 0
+    private var questionIndex = 0
+    private var answerChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answer
+    }
     
     
     
@@ -40,10 +44,59 @@ class QuestionViewController: UIViewController {
 
 extension QuestionViewController {
     private func updateUI() {
+        // Hide stacks
         for stackView in [singleStackView, multipleStackVIew, rangedStackView] {
             stackView?.isHidden = true
         }
-        let currentQuestion = questions[currentIndex]
+        // get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // set current question for label
+        questionLabel.text = currentQuestion.text
+        
+        // calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // set progress for progreeView
+        progressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // show current StackView
+    }
+    
+    private func showCurrentStackView(for type: ResponseType) {
+        switch type {
+        case .single:
+            showSingleStackView(with: currentAnswers)
+        case .multiple:
+            break
+        case .range:
+            break
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.text, for: .normal)
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackVIew.isHidden = false
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.text
+        }
+    }
+    
+    private func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden = false
+        
+        rangedLabels.first?.text = answers.first?.text
+        rangedLabels.last?.text = answers.last?.text
     }
     
 }
