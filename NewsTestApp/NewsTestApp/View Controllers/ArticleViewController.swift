b431d65cc2f9997b3adf8a21debb61ca39039322
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-23 18:33:23 +0300

    add navigation VC to Tab Bar view

---
+++ NewsTestApp/NewsTestApp/View Controllers/ArticleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleViewController.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 23.02.2023.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
