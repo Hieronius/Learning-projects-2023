6bfc70d2f1bcb560ad4e80d286a87a8390337677
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-24 20:45:16 +0300

    Import MapView

--- NewsTestApp/NewsTestApp/View Controllers/MapViewController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/MapViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
-
-// here should be import of MapKit and defining view as mapView
+import CoreLocation
+import MapKit
 
 class MapViewController: UIViewController {
     
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
     }
 }
