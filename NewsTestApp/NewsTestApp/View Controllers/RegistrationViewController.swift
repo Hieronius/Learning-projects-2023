caa76208be7e9a34d00bc8b2c40c3ad3cf807bdb
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-24 15:35:09 +0300

    add outlets and actions for Registration View

--- NewsTestApp/NewsTestApp/View Controllers/RegistrationViewController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/RegistrationViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
     
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // some code here
+        nameTextField.layer.cornerRadius = 20
+        nameTextField.layer.borderWidth = 0.5
+        nameTextField.layer.borderColor = UIColor.white.cgColor
+        nameTextField.layer.masksToBounds = true
+        
+        emailTextField.layer.cornerRadius = 20
+        emailTextField.layer.borderWidth = 0.5
+        emailTextField.layer.borderColor = UIColor.white.cgColor
+        emailTextField.layer.masksToBounds = true
+        
+        passwordTextField.layer.cornerRadius = 20
+        passwordTextField.layer.borderWidth = 0.5
+        passwordTextField.layer.borderColor = UIColor.white.cgColor
+        passwordTextField.layer.masksToBounds = true
+        
+        logInButton.layer.cornerRadius = 20
         
     }
+    
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "TabBarSegue", sender: sender)
+    }
+    
 }
