e39c6c056433a4727394d1b464f28bd3f5a51eda
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-24 21:43:24 +0300

    Add pins to MapView

---
+++ NewsTestApp/NewsTestApp/Model/Coordinates.swift
@@ -0,0 +1,19 @@
+//
+//  Coordinates.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 24.02.2023.
+//
+
+import Foundation
+
+struct Coordinates {
+    let latitude: Double
+    let longtitude: Double
+}
+
+let towerUnderWater = Coordinates(latitude: 37.536791, longtitude: 55.746854)
+let eyeTowerSecond = Coordinates(latitude: 37.536018, longtitude: 55.750457)
+let nevaTower = Coordinates(latitude: 37.533724, longtitude: 55.751133)
+let eurasia = Coordinates(latitude: 37.535466, longtitude: 55.748914)
+let eyeTower = Coordinates(latitude: 37.534406, longtitude: 55.750028)
--- NewsTestApp/NewsTestApp/View Controllers/MapViewController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/MapViewController.swift
@@ -6,10 +6,10 @@
 //
 
 import UIKit
-import CoreLocation
 import MapKit
 
 class MapViewController: UIViewController {
+    
     
     let mapView: MKMapView = {
         let mapView = MKMapView()
@@ -17,9 +17,52 @@ class MapViewController: UIViewController {
         return mapView
     }()
     
+    let nevaTowerPoint: MKPointAnnotation = {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: nevaTower.longtitude, longitude: nevaTower.latitude)
+        return annotation
+        
+    }()
+    
+    let eyeTowerPoint: MKPointAnnotation = {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: eyeTower.longtitude, longitude: eyeTower.latitude)
+        return annotation
+        
+    }()
+    
+    
+    let eyeTowerSecondPoint: MKPointAnnotation = {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: eyeTowerSecond.longtitude, longitude: eyeTowerSecond.latitude)
+        return annotation
+        
+    }()
+    
+    let towerUnderWaterPoint: MKPointAnnotation = {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: towerUnderWater.longtitude, longitude: towerUnderWater.latitude)
+        return annotation
+        
+    }()
+    
+    let eurasiaPoint: MKPointAnnotation = {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: eurasia.longtitude, longitude: eurasia.latitude)
+        return annotation
+        
+    }()
+    
+    var pointsArray: [MKPointAnnotation] {
+        [eyeTowerPoint, eyeTowerSecondPoint, nevaTowerPoint, eurasiaPoint, towerUnderWaterPoint]
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // add function to hide this code
         view.addSubview(mapView)
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
@@ -27,5 +70,7 @@ class MapViewController: UIViewController {
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
+        mapView.addAnnotations(pointsArray)
+        
     }
 }
