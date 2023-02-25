1c5da512f4c0c3f4504f9ccfffb25a7e93f74407
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-25 14:58:37 +0300

    add TableViewCell

---
+++ NewsTestApp/NewsTestApp/Model/FeedViewModel/FeedAPIManager.swift
@@ -0,0 +1,16 @@
+//
+//  FeedAPIManager.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 25.02.2023.
+//
+
+import UIKit
+
+class Post {
+    let date = "14 февраля"
+    let label = "Название"
+    let text = "Тут вы можете прочесть подробное описание"
+    let image = UIImage(named: "testImage")
+    var isLiked = false
+}
---
+++ NewsTestApp/NewsTestApp/View Controllers/Feed/FeedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FeedViewController.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 23.02.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
