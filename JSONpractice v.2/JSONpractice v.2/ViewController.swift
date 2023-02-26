c1a0f57538efcd6ce5529fecb792c4c6e5656fb3
Arsenty Halimovsky <ghost12@yandex.ru>
2023-02-26 22:32:43 +0300

    Create a new project

---
+++ JSONpractice v.2/JSONpractice v.2/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  JSONpractice v.2
+//
+//  Created by Арсентий Халимовский on 26.02.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        // tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    // MARK: - Private properties
+    private var tableView = UITableView()
+
+
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+    
+}
