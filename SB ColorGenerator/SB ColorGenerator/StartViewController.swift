47d2f7a717f27dfb8960e0cff8986f65efe419aa
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-01 17:16:15 +0300

    Add VC

---
+++ SB ColorGenerator/SB ColorGenerator/StartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartViewController.swift
+//  SB ColorGenerator
+//
+//  Created by Арсентий Халимовский on 28.02.2023.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
