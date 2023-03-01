3548368dcced879c2cb6b132d1eb87fc76cd0869
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-01 23:15:33 +0300

    Add VC and delegation protocol

--- delegatePractice/delegatePractice/MainViewController.swift
+++ delegatePractice/delegatePractice/MainViewController.swift
@@ -2,21 +2,49 @@
 
 import UIKit
 
+
+
+protocol MainViewControllerDelegate: AnyObject {
+    func getAge(age: String)
+    func getName(name: String)
+}
+
 class MainViewController: UIViewController {
     
     
-    @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // some code here
+        // some code
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ageSegue" || segue.identifier == "nameSegue" {
+            let secondVC = segue.destination as? SecondViewController
+            secondVC?.mainViewControllerDelegate = self
+        }
     }
     
     
-    @IBAction func checkButtonPressed(_ sender: Any) {
-        
-        performSegue(withIdentifier: "showSecond", sender: sender)
+    @IBAction func ageButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "ageSegue", sender: sender)
     }
     
+    
+    @IBAction func nameButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "nameSegue", sender: sender)
+    }
+}
+
+
+extension MainViewController: MainViewControllerDelegate {
+    func getAge(age: String) {
+        ageLabel.text = age
+    }
+    func getName(name: String) {
+        nameLabel.text = name
+    }
 }
--- delegatePractice/delegatePractice/SecondViewController.swift
+++ delegatePractice/delegatePractice/SecondViewController.swift
@@ -1,23 +1,34 @@
-//
-//  SecondViewController.swift
-//  delegatePractice
-//
-//  Created by Арсентий Халимовский on 01.03.2023.
-//
+
+
+
+
 
 import UIKit
 
 class SecondViewController: UIViewController {
     
     
-    @IBOutlet weak var moodTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    weak var mainViewControllerDelegate: MainViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // some code here
+        
     }
     
-    @IBAction func sendButtonPressed(_ sender: Any) {
+    @IBAction func sendAgeButtonPressed(_ sender: Any) {
+        self.mainViewControllerDelegate?.getAge(age: ageTextField.text!)
         self.dismiss(animated: true)
     }
     
+    
+    @IBAction func sendNameButtonPressed(_ sender: Any) {
+        self.mainViewControllerDelegate?.getName(name: nameTextField.text!)
+        self.dismiss(animated: true)
+    }
 }
