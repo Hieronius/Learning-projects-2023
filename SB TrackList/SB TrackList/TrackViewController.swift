e55b136f0333f5f8aaaacece5e3e28dd4b4e527d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-02 18:25:30 +0300

    Small bug fixes of cell

--- SB TrackList/SB TrackList/TrackViewController.swift
+++ SB TrackList/SB TrackList/TrackViewController.swift
@@ -15,10 +15,14 @@ class TrackViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // add system edit bar button
+        navigationItem.leftBarButtonItem = editButtonItem
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let detailVC = segue.destination as! TrackDetailViewController
+        
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
         detailVC.track = trackList[indexPath.row]
         
@@ -47,25 +51,31 @@ class TrackViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
+    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        // delete "delete" button from "edit" mode
+        .none
+    }
 
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        // delete place for possible buttons so the view will not change it's position
+        false
+    }
+    
+    // this method allowes us to touch and grab the element and throw it into another place 
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // sourceIndexPath and destinationIndexPath is already defined as IndexPath data type
+        // 1. grab the element from it's first position
+        let currentTrack = trackList.remove(at: sourceIndexPath.row)
+        // 2. throw this element to the different position
+        trackList.insert(currentTrack, at: destinationIndexPath.row)
+    }
 
 }
 
-//extension ViewController: UITableViewDataSource {
-//
-//    // how much cells you wan't?
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        10
-//    }
-//
-//    // What cell should be displayed on the table view?
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        // create a reusable cell
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        cell.textLabel?.text = "Cell index: \(indexPath.row)"
-//        return cell
-//    }
-//
-//
-//}
+
 
