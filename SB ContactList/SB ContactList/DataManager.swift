7b0c0d310dcb096c4623634a9f439aa717f142ec
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-02 21:39:48 +0300

    Add userData to the TableView

--- SB ContactList/SB ContactList/DataManager.swift
+++ SB ContactList/SB ContactList/DataManager.swift
@@ -5,16 +5,6 @@
 //  Created by Арсентий Халимовский on 02.03.2023.
 //
 
-import Foundation
-
-
-class DataManager {
-    
-    var arrayOfFirstNames = ["Bruce", "John", "Steven", "Aaron", "Tim", "Allan", "Sharon", "Ted", "Carl", "Nicola"]
-    var arrayOfLastNames = ["Butler", "Smith", "Black", "Robertson", "Murphy", "Williams", "isaacson", "Jankin", "Pennyworth", "Dow"]
-    var arrayOfPhoneNumbers = ["432324432", "994321223", "843153855", "129443124", "929329333", "888431234", "761284443", "439322312", "112094323", "9491129393"]
-    var arrayOfEmailAddresses = ["as@ya.ru", "ja@gmail.com", "bl@jur.ru", "gur@yahoo.com", "baba@gmail.com", "qtun@ya.ru", "squt@ya.ru", "malba@gmail.com", "dork@blur.ru", "asu@gava.com"]
-}
 struct Person {
     
     var firstName: String
@@ -22,4 +12,30 @@ struct Person {
     var phoneNumber: String
     var emailAddress: String
     
+}
+
+
+class DataManager {
+    
+    
+    var arrayOfFirstNames = ["Bruce", "John", "Steven", "Aaron", "Tim", "Allan", "Sharon", "Ted", "Carl", "Nicola"]
+    var arrayOfLastNames = ["Butler", "Smith", "Black", "Robertson", "Murphy", "Williams", "isaacson", "Jankin", "Pennyworth", "Dow"]
+    var arrayOfPhoneNumbers = ["432324432", "994321223", "843153855", "129443124", "929329333", "888431234", "761284443", "439322312", "112094323", "9491129393"]
+    var arrayOfEmailAddresses = ["as@ya.ru", "ja@gmail.com", "bl@jur.ru", "gur@yahoo.com", "baba@gmail.com", "qtun@ya.ru", "squt@ya.ru", "malba@gmail.com", "dork@blur.ru", "asu@gava.com"]
+    
+    func getUsers() -> [Person] {
+        
+        var arrayOfUser = [Person]()
+        let numberOfUsers = arrayOfFirstNames.count
+        for person in 1...numberOfUsers {
+            arrayOfUser.append(Person(
+                firstName: arrayOfFirstNames.shuffled()[0],
+                lastName: arrayOfLastNames.shuffled()[0],
+                phoneNumber: arrayOfPhoneNumbers.shuffled()[0],
+                emailAddress: arrayOfEmailAddresses.shuffled()[0]))
+            
+            
+        }
+        return arrayOfUser
+    }
 }
--- SB ContactList/SB ContactList/MainViewTableController.swift
+++ SB ContactList/SB ContactList/MainViewTableController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class MainViewTableController: UITableViewController {
     
+    var usersArray = DataManager().getUsers()
+    
+    
     
     var userName = "Alfred"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(usersArray.count)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -27,12 +31,13 @@ class MainViewTableController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return usersArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Check"
+        let cellData = usersArray[indexPath.row]
+        cell.textLabel?.text = "\(cellData.firstName) \(cellData.lastName)"
         cell.textLabel!.font = UIFont(name: "system", size: 50)
         return cell
     }
