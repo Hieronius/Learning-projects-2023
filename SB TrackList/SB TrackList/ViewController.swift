75f87bf6b70531b78ccd189e5ec33b622ae684eb
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-02 12:06:03 +0300

    Add delegation and data source

--- SB TrackList/SB TrackList/ViewController.swift
+++ SB TrackList/SB TrackList/ViewController.swift
@@ -17,3 +17,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    // how much cells you wan't?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    // What cell should be displayed on the table view?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create a reusable cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+    }
+    
+    
+}
+
