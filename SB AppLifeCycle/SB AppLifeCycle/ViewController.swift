b82eae35d4351b8a556b87fde426566f0ffb9fe5
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-04 17:01:37 +0300

    Add loadView and init

--- SB AppLifeCycle/SB AppLifeCycle/ViewController.swift
+++ SB AppLifeCycle/SB AppLifeCycle/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    // 1. Когда создается обьект, первым срабатывает инициализация
+    // Если создать какой-нибудь оутлет через сториборд, то он будет готов сразу после инициализации
+    // init() { }
+    //
+    
+    // 2. View загружается
+    override func loadView() {
+        super.loadView()
+    }
+    
+    // 3. View загрузились
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
