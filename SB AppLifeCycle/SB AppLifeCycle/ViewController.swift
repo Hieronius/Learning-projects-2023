8b58b2ead808d85414fb76a2c8cec9fe75f91f66
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-04 16:51:42 +0300

    Create a new project

---
+++ SB AppLifeCycle/SB AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SB AppLifeCycle
+//
+//  Created by Арсентий Халимовский on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
