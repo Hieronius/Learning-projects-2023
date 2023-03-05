9b16bf855b4e97b9625b2392a3102f77a4c6afde
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-05 14:58:46 +0300

    add method to move imageView

--- SB AnimationApp/SB AnimationApp/ViewController.swift
+++ SB AnimationApp/SB AnimationApp/ViewController.swift
@@ -8,18 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var coreAnimationView: UIView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
-        sender.pulsate()
+    private var originCoordinate: CGFloat?
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        originCoordinate = coreAnimationView.frame.origin.x
     }
     
-}
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        originCoordinate = coreAnimationView.frame.origin.x
+        print(originCoordinate)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func coreAnimationButtonPressed(_ sender: UIButton) {
+        sender.pulsate()
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.autoreverse, .repeat]) { [weak self] in
+            guard let self = self else { return }
+            
+            if self.coreAnimationView.frame.origin.x == self.originCoordinate {
+                self.coreAnimationView.frame.origin.x -= 40.0
+            }
+            
+        }
+            
+        }
+    }
