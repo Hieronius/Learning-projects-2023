a1706cea679673670a8be5b1881a2c9695a8031d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-08 21:57:11 +0300

    Create collection view cell

---
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FavouriteCollectionViewCell.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 08.03.2023.
+//
+
+import UIKit
+
+class FavouriteCollectionViewCell: UICollectionViewCell {
+    
+}
