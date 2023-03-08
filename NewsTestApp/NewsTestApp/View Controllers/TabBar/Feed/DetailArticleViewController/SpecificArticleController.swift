11f275fca968ecc0163f13ecf1137849154e5071
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-08 17:22:19 +0300

    Create SpecificVC

---
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Feed/DetailArticleViewController/SpecificArticleController.swift
@@ -0,0 +1,17 @@
+//
+//  SpecificArticleController.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 08.03.2023.
+//
+
+import UIKit
+
+class SpecificViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
