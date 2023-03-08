6f9e0e59b054dab9007dcbe07d8d5bb62913b6a9
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-08 17:26:10 +0300

    Add outlets for SpecificVC

--- NewsTestApp/NewsTestApp/View Controllers/TabBar/Feed/DetailArticleViewController/SpecificArticleController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Feed/DetailArticleViewController/SpecificArticleController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class SpecificViewController: UIViewController {
     
+    @IBOutlet weak var specificImageView: UIImageView!
+    @IBOutlet weak var specificDateLabel: UILabel!
+    @IBOutlet weak var specificLikeButton: UIButton!
+    @IBOutlet weak var specificArticleLabel: UILabel!
+    @IBOutlet weak var specificArticleText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
