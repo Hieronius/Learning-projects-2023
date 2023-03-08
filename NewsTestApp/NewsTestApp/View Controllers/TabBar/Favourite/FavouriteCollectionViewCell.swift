b8404ac3ebbb2ae8a9c840173ddd8260458a83b0
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-08 22:00:00 +0300

    add outlets for collection cell

--- NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteCollectionViewCell.swift
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteCollectionViewCell.swift
@@ -9,4 +9,9 @@ import UIKit
 
 class FavouriteCollectionViewCell: UICollectionViewCell {
     
+    
+    @IBOutlet weak var collectionImageView: UIImageView!
+    @IBOutlet weak var collectionDateLabel: UILabel!
+    @IBOutlet weak var collectionArticleLabel: UILabel!
+    @IBOutlet weak var collectionLikeButton: UIButton!
 }
