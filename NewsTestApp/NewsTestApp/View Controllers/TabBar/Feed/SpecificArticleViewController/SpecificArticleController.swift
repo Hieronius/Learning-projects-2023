1876da6d3fc12b09f83c0147fe64a93d459cd606
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-08 21:03:23 +0300

    Add view for image view

--- NewsTestApp/NewsTestApp/View Controllers/TabBar/Feed/SpecificArticleViewController/SpecificArticleController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Feed/SpecificArticleViewController/SpecificArticleController.swift
@@ -14,6 +14,7 @@ class SpecificViewController: UIViewController {
     @IBOutlet weak var specificLikeButton: UIButton!
     @IBOutlet weak var specificArticleLabel: UILabel!
     @IBOutlet weak var specificArticleText: UILabel!
+    @IBOutlet weak var specificView: UIView!
     
     let specificDefaultImage = "https://media.istockphoto.com/photos/generic-red-suv-on-a-white-background-side-view-picture-id1157655660?b=1&k=20&m=1157655660&s=612x612&w=0&h=ekNZlV17a3wd_yN9PhHXtIabO_zFo4qipCy2AZRpWUI="
     
@@ -28,5 +29,8 @@ class SpecificViewController: UIViewController {
         specificArticleText.text = article.content
         specificImageView.loadImage(urlString: article.urlToImage ?? specificDefaultImage)
         specificLikeButton.setImage(UIImage(named: "like"), for: .normal)
+        
+        
+        
     }
 }
