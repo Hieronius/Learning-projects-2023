adc0f0ca30fa1a8e76c558b5635d814475bf020d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-10 16:51:51 +0300

    transfer view controller

--- DetailArticleViewController/DetailArticleViewController/ViewController.swift
+++ DetailArticleViewController/DetailArticleViewController/ViewController.swift
@@ -17,3 +17,60 @@ class ViewController: UIViewController {
 
 }
 
+//
+//  ArticleViewController.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 23.02.2023.
+//
+
+import UIKit
+
+class DetailArticleViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellTwo", for: indexPath) as! DetailTableViewCell
+        
+        cell.detailImageView.image = UIImage(named: "image")
+        cell.detailDateLabel.text = "14 февраля"
+        cell.detailLikeButton.imageView?.image = UIImage(named: "likeImage")
+        cell.detailArticleLabel.text = "Название"
+        cell.detailArticleText.text = """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proid
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        """
+        return cell
+    }
+    
+}
+
+//
+//  DetailTableViewCell.swift
+//  NewsTestApp
+//
+//  Created by Арсентий Халимовский on 07.03.2023.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var detailDateLabel: UILabel!
+    @IBOutlet weak var detailLikeButton: UIButton!
+    @IBOutlet weak var detailArticleLabel: UILabel!
+    @IBOutlet weak var detailArticleText: UILabel!
+    
+}
+
+
