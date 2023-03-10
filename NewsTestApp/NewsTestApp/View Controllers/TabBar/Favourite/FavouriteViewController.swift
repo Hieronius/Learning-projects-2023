9afc44e57ed9f9bf4ed25c68805af5654d828c46
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-10 16:33:02 +0300

    fix collection view

--- NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteViewController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class FavouriteViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class FavouriteViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var articles: [Article]!
     
@@ -15,15 +18,30 @@ class FavouriteViewController: UIViewController, UICollectionViewDelegate, UICol
         super.viewDidLoad()
         
         // some code here
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        3
+        print("10 items have been created")
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection", for: indexPath)
-        collectionCell.
+        let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection", for: indexPath) as! FavouriteCollectionViewCell
+        collectionCell.collectionDateLabel.text = "14 february"
+        collectionCell.collectionArticleLabel.text = "Lorem ipsum dolor sit amet."
+        collectionCell.collectionImageView.image = UIImage(named: "Image")
+        collectionCell.collectionLikeButton.setImage(UIImage(named: "likePressed"), for: .normal)
+        print("cell has beed created")
         return collectionCell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 164, height: 191)
+    }
+    
 }
+
+
