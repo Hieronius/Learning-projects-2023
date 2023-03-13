f2704889779e8c3d1c57c94f73b0d16327545d2c
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-13 17:34:30 +0300

    add method to send articles to FeedVC
    From FavouriteVC

--- NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteViewController.swift
+++ NewsTestApp/NewsTestApp/View Controllers/TabBar/Favourite/FavouriteViewController.swift
@@ -31,41 +31,77 @@ class FavouriteViewController: UIViewController, UICollectionViewDelegate, UICol
     @IBAction func favouriteLikeButtonPressed(_ sender: UIButton) {
         
         
+        // get access to selected article
+        let likedArticleIndex = sender.tag
+        let likedArticle = articles[likedArticleIndex]
         
+        // add or remove selected articles from savedArticles array
         
         
         if sender.imageView?.image == UIImage(named: "like") {
             sender.setImage(UIImage(named: "likePressed"), for: .normal)
             print("Favourite like button has been pressed")
-
+            
+            articles.append(likedArticle)
+            
         } else {
+            
             sender.setImage(UIImage(named: "like"), for: .normal)
             print("Favourite dislike button has been pressed")
+            
+            // if there is like button already pressed and we wan't to remove article
+            if let index = articles.firstIndex(of: likedArticle) {
+                self.articles.remove(at: index)
+                print(articles.count)
+            }
         }
+        
+        // send current array of the articles to the FeedViewController
+        
+        // define tab bar
+        let tabBar = self.tabBarController
+        
+        // check is there is a view controllers or nil
+        guard let viewControllers = tabBar?.viewControllers else { return }
+        
+        // get access to the Navigation View Controller which is connected to the specific ViewController
+        for viewController in viewControllers {
+            
+            if let feedNaviVC = viewController as? FeedNavigationViewController {
+                
+                // if our way to the NavigationViewController is succesful let's try to get viewController
+                if let feedVC = feedNaviVC.viewControllers.first as? FeedViewController {
+                    
+                    print("The article has been saved")
+                    feedVC.savedArticles = self.articles
+                    print(feedVC.savedArticles.count)
+                }
+            }
+        }
+        
+    }
+        
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            print(articles.count)
+            return articles.count
+        }
+        
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection", for: indexPath) as! FavouriteCollectionViewCell
+            collectionCell.collectionDateLabel.text = articles[indexPath.row].publishedAt
+            collectionCell.collectionArticleLabel.text = articles[indexPath.row].title
+            collectionCell.collectionImageView.loadImage(urlString: articles[indexPath.row].urlToImage ?? favouriteDefaultImage)
+            collectionCell.collectionLikeButton.setImage(UIImage(named: "likePressed"), for: .normal)
+            print("cell has beed created")
+            collectionCell.layer.cornerRadius = 20
+            return collectionCell
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 164, height: 191)
+        }
+        
     }
     
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(articles.count)
-        return articles.count
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection", for: indexPath) as! FavouriteCollectionViewCell
-        collectionCell.collectionDateLabel.text = articles[indexPath.row].publishedAt
-        collectionCell.collectionArticleLabel.text = articles[indexPath.row].title
-        collectionCell.collectionImageView.loadImage(urlString: articles[indexPath.row].urlToImage ?? favouriteDefaultImage)
-         collectionCell.collectionLikeButton.setImage(UIImage(named: "likePressed"), for: .normal)
-        print("cell has beed created")
-        collectionCell.layer.cornerRadius = 20
-        return collectionCell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 164, height: 191)
-    }
-    
-}
-
-
